d8ee7df0abdf947272f16967111d1180340663db
isimmons <isimmons33@gmail.com>
2024-05-02 01:35:11 -0400

    starter code for working with postman intro

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ApiLoginRequest;
 use App\Traits\ApiResponses;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
@@ -11,10 +12,19 @@ class AuthController extends Controller
     use ApiResponses;
 
     /**
+     * Authenticate a registered user
+     * @param ApiLoginRequest $request
      * @return JsonResponse
      */
-    public function login()
+    public function login(ApiLoginRequest $request): JsonResponse
     {
-        return $this->ok('Welcome to the API!');
+        return $this->ok($request->get('email'));
     }
+
+    public function register()
+    {
+        return $this->ok('registered');
+    }
+
+
 }
--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,8 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/register', [AuthController::class, 'register'])->name('register');
 
 Route::get('/user', function (Request $request) {
     return $request->user();
