3a70e6ef2826a12feab4d4da472cba2e3baa62f5
isimmons <isimmons33@gmail.com>
2024-05-06 21:44:56 -0400

    prepare for distiquish between users and authors

--- app/Http/Controllers/Api/V1/AuthorController.php
+++ app/Http/Controllers/Api/V1/AuthorController.php
@@ -8,7 +8,7 @@ use App\Http\Requests\Api\V1\UpdateUserRequest;
 use App\Http\Resources\V1\UserResource;
 use App\Models\User;
 
-class UserController extends ApiController
+class AuthorController extends ApiController
 {
     /**
      * Display a listing of the resource.
@@ -34,13 +34,13 @@ class UserController extends ApiController
     /**
      * Display the specified resource.
      */
-    public function show(User $user)
+    public function show(User $author)
     {
         if($this->include('tickets')) {
-            return new UserResource($user->load(['tickets']));
+            return new UserResource($author->load(['tickets']));
         }
 
-        return new UserResource($user);
+        return new UserResource($author);
     }
 
 
--- routes/api_v1.php
+++ routes/api_v1.php
@@ -1,8 +1,8 @@
 <?php
 
 use App\Http\Controllers\Api\V1\TicketController;
-use App\Http\Controllers\Api\V1\UserController;
+use App\Http\Controllers\Api\V1\AuthorController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->apiResource('tickets', TicketController::class);
-Route::middleware('auth:sanctum')->apiResource('users', UserController::class);
+Route::middleware('auth:sanctum')->apiResource('authors', AuthorController::class);
